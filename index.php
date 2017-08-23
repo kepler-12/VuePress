1e6a8fe4e07872786f1f9d48a00b8563ff179b4c
zackkrida <zackwcote@gmail.com>
2017-08-23 13:37:14 -0400

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="app"></div>
+</body>
+<?php wp_footer(); ?>
+
+</body>
+</html>
