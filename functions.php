64ce68b3c0d58c80246a767a50e72785add91f87
zackkrida <zackwcote@gmail.com>
2017-09-21 14:55:24 -0400

    update redirects

--- functions.php
+++ functions.php
@@ -6,6 +6,17 @@ add_theme_support('post-thumbnails');
 // stop redirection
 remove_action('template_redirect', 'redirect_canonical');
 
-// Allow anyone to comment as much as they want :D
+// TODO: Remove when authentication is completed
 add_filter('rest_allow_anonymous_comments', '__return_true');
+
+// Allow users to comment as much as they want :D
 add_filter('comment_flood_filter', '__return_false');
+
+// Redirect all user-facing (non admin area) pages to '/'
+// (Vue.js is handling routing now)
+add_filter('template_redirect', function () {
+    if (!is_admin()) {
+        wp_redirect(home_url());
+        exit;
+    }
+});
