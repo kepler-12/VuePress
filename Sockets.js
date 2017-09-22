e264d36de09c91de6ecce16966f963b8a4a296eb
Tyler Smith <t@q1s.co>
2017-09-22 10:40:29 -0400

    Standardize Socket.js

--- Sockets.js
+++ Sockets.js
@@ -5,46 +5,14 @@ require('dotenv').config()
 // For Incoming and Outgoing connections
 const io = require('socket.io')(http);
 
-//For all HTTP Requests
-const axios = require('axios');
-
-//The base url for all of our axios requests
-const baseUrl = `${process.env.BASE_URL}/wp-json/wp/v2/`
-console.log(baseUrl);
 //Instatiate a socket.io connection
 io.on('connection', function (socket) {
-  //Listen for the event 'wp-rest' and preform function
-  socket.on('wp-rest', ({ method, path, data, response }) => {
-    const url = baseUrl + path
-    //Submit the ajax request to the wordpress restapi
-    axios({
-      method,
-      url,
-      data
-    })
-      .then(emit(response))
-      .catch(emitError('error'))
-  });
-  socket.on('update', ({response, data}) => {
-    emit(response)({data});
+  socket.on('event', ({response, data}) => {
+    io.emit(response, data);
   })
 });
 
-// function for emmiting to all sockets currently connected to this port
-function emit(path) {
-  return function (res) {
-    console.log(path, res.data);
-    return io.emit(path, res.data)
-  }
-}
 
-function emitError(path) {
-  return function (res) {
-    console.log(path, res);
-    return io.emit(path, res)
-  }
-}
-
-http.listen(3020, function () {
-  console.log('listening on *:3020');
+http.listen(process.env.PORT, function () {
+  console.log(`listening on *:${process.env.PORT}`);
 })
